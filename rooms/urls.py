04079aec9b8b72bcf8266558a66396675854baa4
emp0107 <pandoemp0107@pandoconsultants.com>
2023-12-14 11:18:14 +0530

    profile and search profile  api created

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,6 +1,6 @@
 # rooms/urls.py
 from django.urls import path, include
-from .views import RoomListCreateView, UserViewSet, MyInbox
+from .views import RoomListCreateView, UserViewSet, MyInbox, GetMessages, SendMessage, SearchUser, ProfileDetails
 from rest_framework.routers import DefaultRouter
 from .consumers import ChatConsumer
 
@@ -12,5 +12,11 @@ urlpatterns = [
     path('rooms/', RoomListCreateView.as_view(), name='room-list-create'),
     path('ws/chat/<str:room_name>/', ChatConsumer.as_asgi(), name='chat-room'),
     path('api/', include(router.urls)), 
-    path("my-messages/<user_id>/", MyInbox.as_view())
+    # chat messages api
+    path('my-messages/<user_id>/', MyInbox.as_view()),
+    path('get-messages/<sender_id>/<receiver_id>/', GetMessages.as_view()),
+    path('send-messages/', SendMessage.as_view()),
+    # profile details
+    path('profile/<int:pk>/',ProfileDetails.as_view()),
+    path('profile/search/<username>/',SearchUser.as_view())
 ]
